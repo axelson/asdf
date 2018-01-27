d1ead106176e029e9bda4fda9f49850242caa701
Jason Axelson <jason.axelson@gmail.com>
2018-01-27 10:36:41 -1000

    Source commands

--- lib/commands/reinstall.sh
+++ lib/commands/reinstall.sh
@@ -1,3 +1,8 @@
+# shellcheck source=lib/commands/install.sh
+source "$(dirname "$(dirname "$0")")/lib/commands/install.sh"
+# shellcheck source=lib/commands/uninstall.sh
+source "$(dirname "$(dirname "$0")")/lib/commands/uninstall.sh"
+
 reinstall_command() {
   local plugin_name=$1
   local full_version=$2
@@ -6,11 +11,7 @@ reinstall_command() {
     display_error "You must specify a name and a version to reinstall"
     exit 1
   else
-    reinstall_tool_version "$plugin_name" "$full_version"
+    uninstall_command "$plugin_name" "$full_version"
+    install_command "$plugin_name" "$full_version"
   fi
-}
-
-reinstall_tool_version() {
-  uninstall_command()
-  install_command()
 }
